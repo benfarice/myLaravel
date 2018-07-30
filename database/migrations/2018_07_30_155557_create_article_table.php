1a59f5140e986af43e52a8627b89290d4208b222
benfarice <imzoughene@outlook.com>
2018-07-30 17:12:55 +0100

    Système de Migrations

--- database/migrations/2018_07_30_155557_create_article_table.php
+++ database/migrations/2018_07_30_155557_create_article_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateArticleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-       /*
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('titre',67);
+            $table->text('contenu');
+            $table->timestamp('date_publication');
             $table->timestamps();
         });
-        */
     }
 
     /**
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
